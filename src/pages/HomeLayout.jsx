366665efe46fe7f66c5f6892fd1ae2662fc95924
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-05 22:32:50 -0500

    Added contents and CSS for landing page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
     <div>
-        <nav>
-            navbar
-        </nav>
-        <Outlet />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
