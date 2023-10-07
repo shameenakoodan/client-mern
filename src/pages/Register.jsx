3eaaf050c02dd3b9d90ec235e36308a6de81ebbd
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-06 23:28:46 -0500

    Added the contents for the register page with style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,31 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import Logo from "../components/Logo";
+import FormRow from "../components/FormRow";
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1> 
-      <Link to="/login">Login Page</Link>
-    </div>
-  )
-}
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' defaulValue={"John"}/>
+        <FormRow type='text' name='lastName' labelText={"Last Name"}defaulValue={"Smith"}/>
+        <FormRow type='text' name='location' defaulValue={"earth"}/>
+        <FormRow type='email' name='email' defaulValue={"john@gmail.com"}/>
+        <FormRow type='password' name='password' defaulValue={"secret123"}/>
 
-export default Register
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Register;
