d8da221510bcda1a1bb10461d77ac0079ac66186
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-07 13:11:12 -0500

    Added the navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import {FaHome} from 'react-icons/fa';
+import {FaAlignLeft} from 'react-icons/fa';
+import Logo from "./Logo";
+import { useDashboardContext } from "../pages/DashboardLayout";
 const Navbar = () => {
+  const{toggleSidebar} = useDashboardContext();
   return (
     <Wrapper>
-        <FaHome />
+        <div className="nav-center">
+          <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+            <FaAlignLeft />
+          </button>
+          <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+          </div>
+          <div className="btn-container">
+            toggle/logout
+          </div>
+          </div>
         </Wrapper>
   )
 }
