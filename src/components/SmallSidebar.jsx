fe24584734507273c2c88b0d0c16ec10aa861c42
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-09 08:19:21 -0500

    Added the sidebars for small and big screens

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,39 +1,30 @@
-import { FaTimes } from "react-icons/fa";
-import Wrapper from "../assets/wrappers/SmallSidebar";
-import { useDashboardContext } from "../pages/DashboardLayout";
-import Logo from "./Logo";
-import { NavLink } from "react-router-dom";
-import links from "../utils/links";
-const SmallSidebar = () => {
-  const {showSidebar,toggleSidebar} = useDashboardContext();
-  return (
+import { FaTimes } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
 
+import NavLinks from './NavLinks';
+const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+
+  return (
     <Wrapper>
-      <div className={
-        showSidebar ? 
-        "sidebar-container show-sidebar":
-        'sidebar-container'}>
-        <div className="content">
-          <button type="button" className="close-btn" onClick={toggleSidebar}>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
             <FaTimes />
           </button>
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link)=>{
-                const {text,path,icon} = link;
-                return (
-                  <NavLink to={path} key={text} className='nav-link' onClick={toggleSidebar}>
-                    <span className="icon">{icon}</span>
-                  </NavLink>
-                );
-            })}
-            </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
-  )
-}
-
-export default SmallSidebar
+  );
+};
+export default SmallSidebar;
